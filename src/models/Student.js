020042c98dfae295b4e047f16058439a12442a52
Victor Fuentes <victor.velazfuentez@gmail.com>
2020-05-21 16:09:10 -0500

    Todo Bien

--- src/models/Student.js
+++ src/models/Student.js
@@ -2,78 +2,90 @@ import uuid from 'uuid';
 import DBManager from '../managers/DBManager';
 
 const studentSchema = {
-   id: {
-      type: String,
-      hashKey: true
-   },
-   firtName: String,
-   lastName: String,
-   major: String,
-   semester: Number,
-   createdAt: String,
-   updatedeAt: String
+  id: {
+    type: String,
+    hashKey: true
+  },
+  firtName: String,
+  lastName: String,
+  major: String,
+  semester: Number,
+  createdAt: String,
+  updatedeAt: String
 };
 
-export default class Student extends DBManager{
-   id;
-   firstName;
-   lastName;
-   major;
-   semester;
-   createdAt;
-   updatedeAt;
+export default class Student extends DBManager {
+  id;
 
-   constructor(
-      id, firstName, lastName, major, semester, createdAt, updatedeAt
-   ){
-      super(process.env.STUDENT_TABLE_NAME, studentSchema)
+  firstName;
 
+  lastName;
 
-      this.id= id;
-      this.firstName= firstName;
-      this.lastName= lastName;
-      this.major= major;
-      this.semester= semester;
-      this.createdAt= createdAt;
-      this.updatedeAt= updatedeAt;
-   }
+  major;
 
-   toDBFormat(){
-      return{
-      ...this, 
+  semester;
+
+  createdAt;
+
+  updatedeAt;
+
+  constructor(id, firstName, lastName, major, semester, createdAt, updatedeAt) {
+    super(process.env.STUDENT_TABLE_NAME, studentSchema);
+
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.major = major;
+    this.semester = semester;
+    this.createdAt = createdAt;
+    this.updatedeAt = updatedeAt;
+  }
+
+  toDBFormat() {
+    return {
+      ...this,
       updatedeAt: this.updatedeAt.ToString(),
       createdAt: this.createdAt.ToString()
-      };
-   }
+    };
+  }
 
-   getKey(){
-      returnthis.id;
-   }
+  getKey() {
+    return this.id;
+  }
 
-   fromDBResponse(item){
-      const{
-         id,
-         firstName,
-         lastName,
-         major,
-         semester,
-         createdAt,
-         updatedeAt
-      } = item;
+  // eslint-disable-next-line class-methods-use-this
+  fromDBResponse(item) {
+    const {
+      id,
+      firstName,
+      lastName,
+      major,
+      semester,
+      createdAt,
+      updatedeAt
+    } = item;
 
-      return new Student(id, firstName, lastName, major, semester, createdAt, updatedeAt);
-   }
+    return new Student(
+      id,
+      firstName,
+      lastName,
+      major,
+      semester,
+      createdAt,
+      updatedeAt
+    );
+  }
 
-    static newStudent(firstName, lastName, major, semester){
-       const id = uuid();
-       return new Student(
-          id, 
-          firstName,
-          lastName,
-          major,
-          semester,
-          new Date(),
-          new Date()
-       );
-    }
+  static newStudent(firstName, lastName, major, semester) {
+    const id = uuid();
+    return new Student(
+      id,
+      firstName,
+      lastName,
+      major,
+      semester,
+      new Date(),
+      new Date()
+    );
+  }
 }
